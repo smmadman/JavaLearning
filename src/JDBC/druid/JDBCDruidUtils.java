be635e48eb9dd07e6cb793ddb496396dcee5f285
王俊杰（实验室） <1648177163@qq.com>
2020-07-01 14:42:51 +0800

    Add Druid Connection resources.

---
+++ src/JDBC/druid/JDBCDruidUtils.java
@@ -0,0 +1,104 @@
+package JDBC.druid;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import org.apache.commons.dbutils.DbUtils;
+
+import javax.sql.DataSource;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class JDBCDruidUtils {
+    /**
+     * 使用Druid数据库连接池技术
+     */
+    private static DataSource source1;
+    static{
+        try {
+            Properties pros = new Properties();
+
+            InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("druid.properties");
+
+            pros.load(is);
+
+            source1 = DruidDataSourceFactory.createDataSource(pros);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConnection3() throws SQLException {
+
+        Connection conn = source1.getConnection();
+        return conn;
+    }
+
+    /**
+     *
+     * @Description 关闭连接和Statement的操作
+     * @author shkstart
+     * @date 上午9:12:40
+     * @param conn
+     * @param ps
+     */
+    public static void closeResource(Connection conn, Statement ps){
+        try {
+            if(ps != null)
+                ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if(conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     *
+     * @Description 关闭资源操作
+     * @author shkstart
+     * @date 上午10:21:15
+     * @param conn
+     * @param ps
+     * @param rs
+     */
+    public static void closeResource(Connection conn, Statement ps, ResultSet rs){
+        try {
+            if(ps != null)
+                ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if(conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if(rs != null)
+                rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * @Description 使用dbutils.jar中提供的DbUtils工具类，实现资源的关闭
+     * @author shkstart
+     * @date 下午4:53:09
+     * @param conn
+     * @param ps
+     * @param rs
+     */
+    public static void closeResource1(Connection conn,Statement ps,ResultSet rs){
+        DbUtils.closeQuietly(conn);
+        DbUtils.closeQuietly(ps);
+        DbUtils.closeQuietly(rs);
+    }
+}
